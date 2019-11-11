4ea501a1c0a79c80562d8a071d67fb860b8f3c49
erich-verys <56269292+erich-verys@users.noreply.github.com>
2019-11-11 10:00:30 -0800

    initial commit

--- lesson-15-throttlingandbackpressure/server/server.go
+++ lesson-15-throttlingandbackpressure/server/server.go
@@ -1,1 +1,18 @@
+package server
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func Run() {
+	fmt.Println("starting server at :8888")
+	http.HandleFunc("/status", status)
+	http.ListenAndServe(":8888", nil)
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("request received")
+	w.WriteHeader(200)
+	w.Write([]byte(`{"success":true}`))
+}
